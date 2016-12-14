dccbb75023d2d50d072979d4d6b4d535b71fb417
keisuke umezawa <keisuke-umezawa@users.noreply.github.com>
2016-12-14 22:55:05 +0900

    Update linker.sh

--- linker.sh
+++ linker.sh
@@ -1,1 +1,16 @@
-ln -sf ~/dot/.vimrc ~/.vimrc
+#!/bin/sh
+
+DOTFILES_DIR=$(cd $(dirname $0) && pwd)
+
+# bash settings
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
+ln -sf ${DOTFILES_DIR}/.bash_profile ~/.bash_profile
+ln -sf ${DOTFILES_DIR}/.bashrc ~/.bashrc
+
+# vim settings
+ln -sf ${DOTFILES_DIR}/.vimrc ~/.vimrc
+if [ ! -L ~/.vim ]; then
+	rm -rf ~/.vim
+fi
+ln -snf ${DOTFILES_DIR}/.vim/ ~/.vim
+
